e8cde58dbb8f0cb64036fff6a7cce5ba67835861
Dhaval <hey.dakshv@gmail.com>
2026-01-26 16:23:32 +0530

    feat: Migrate Redis configuration to use `@tradevia/config` instead of `dotenv`.

--- packages/redis/index.ts
+++ packages/redis/index.ts
@@ -1,14 +1,13 @@
-import "dotenv/config";
+import { config } from "@tradevia/config";
 import Redis from "ioredis";
 
-const redis =
-    process.env.REDIS_URL
-        ? new Redis(process.env.REDIS_URL)
-        : new Redis({
-            host: process.env.REDIS_HOST || "redis",
-            port: Number(process.env.REDIS_PORT || 6379),
-            password: process.env.REDIS_PASSWORD,
-        });
+const redis = config.redis_url
+    ? new Redis(config.redis_url)
+    : new Redis({
+        host: process.env.REDIS_HOST || "redis",
+        port: Number(process.env.REDIS_PORT || 6379),
+        password: process.env.REDIS_PASSWORD,
+    });
 
 
 export { redis };
