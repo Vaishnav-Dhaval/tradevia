9bd7f49fce6c5011ac5e5e14665ec60425d2a333
Dhaval <hey.dakshv@gmail.com>
2026-01-26 14:51:40 +0530

    added cors url

--- apps/api-service/src/index.ts
+++ apps/api-service/src/index.ts
@@ -16,10 +16,10 @@ app.use(
       "http://localhost:3200",
       "http://localhost:3000",
       "http://localhost:3001",
-      "https://exness.dakshv.in",
-      "https://www.exness.dakshv.in",
-      "http://exness.dakshv.in",
-      "http://www.exness.dakshv.in"
+      "https://api.exness.dakshv.in",
+      "http://api.exness.dakshv.in",
+      "https://www.api.exness.dakshv.in",
+      "http://www.api.exness.dakshv.in"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
